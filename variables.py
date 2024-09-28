af6343479df0bd1bbc283a355c953fbfa064d7c4
DmitryVybornov <dymrulyt@gmail.com>
2024-09-28 03:09:13 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+home_work =  12
+time_spent = 1.5
+course_name = 'Python'
+time_on_task = time_spent / home_work
+print('Курс:',course_name,', всего задач:', home_work, ', затрачено часов:', time_spent, ', среднее время выполнения', time_on_task, 'часа.'  )
